c876be311ca64f8482d99941ad04f34d0d251c45
ItsTRD <trdeshmukh.2011@gmail.com>
2021-04-30 01:38:50 +0530

    Code V 1.1

--- AI-SPEECH-BOT/main.py
+++ AI-SPEECH-BOT/main.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template, request
 import numpy,random
-import speech_recognition as sr
+#import speech_recognition as sr
 import chatbot as bot
 app = Flask(__name__)
 
-#For voice recording
-r = sr.Recognizer()
-mic = sr.Microphone()
 
-# english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
-# trainer = ChatterBotCorpusTrainer(english_bot)
-# trainer.train("chatterbot.corpus.english")
+current_user = "Jane"
+
+questions_db = {"Syntax Error": [3, "Q1", "Q2", "Q3"]}
+
+
 
 
 @app.route("/")
@@ -18,25 +17,23 @@ def home():
     return render_template("template_for_testing.html")
     #return render_template("not_used_home.html")
 
-
-@app.route("/voice")
-def get_voice_response():
+@app.route("/saveAnswer")
+def get_bot_response_save_answer():
     userText = request.args.get('msg')
-    with mic as source:
-        print("Speak anything")
-        r.adjust_for_ambient_noise(source)
-        audio = r.listen(source)
+    print("saving : ", userText)
+    return "Saved"
 
-    try:
-        text = r.recognize_google(audio)
-        print("You said", str(text))
-        return (str(text))
-
-    except Exception as Error:
-        print(Error)
-        return  Error
-
-    return ("Code error : Try Block Not Executed")
+@app.route("/ask")
+def get_bot_question():
+    userText = request.args.get('msg')
+    tag = request.args.get('question_tag')
+    question_no = request.args.get('question_number')
+    print("msg", userText)
+    print("Question No",question_no)
+    print("TAG ", tag)
+    print("tag in db", tag in questions_db)
+    print(questions_db[tag][int(question_no)])
+    return questions_db[tag][int(question_no)]
 
 @app.route("/get")
 def get_bot_response():
@@ -47,7 +44,9 @@ def get_bot_response():
 
         results = bot.model.predict([bot.bag_of_words(userText, bot.words)])[0]
         results_index = numpy.argmax(results)
-
+        
+     
+        
         tag = bot.labels[results_index]
 
         if results[results_index] > 0.7:
@@ -55,21 +54,26 @@ def get_bot_response():
             for tg in bot.data["intents"]:
                 if tg['tag'] == tag:
                     responses = tg['responses']
+                    responses = random.choice(responses)
+                    if tag in questions_db:
+                        responses += "|true|" + str(questions_db[tag][0]) + "|" + tag
+                    else:
+                        responses += "|false|0|empty"
 
             global output
-            output=(random.choice(responses))
+            output=responses
         else:
-            output="I didn't get that plz try try reframing your query else raise a new error registration request from the pane above"
+            output="I didn't get that plz try try reframing your query else raise a new error registration request from the pane above|false|0|empty"
 
         print(str(output))
         return (str(output))
 
     except Exception as Error:
-        print(Error)
+        print(Error) 
         return  Error
 
     return userText
-    return str(english_bot.get_response(userText))
+    
 
 
 
